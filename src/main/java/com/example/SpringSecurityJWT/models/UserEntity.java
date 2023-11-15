424f4bf47ddfff5478d3304e85d6b53cb958eb86
Jonathan <Jonathan@192.168.56.1>
2023-11-15 08:53:53 -0500

    Todo excelente

--- src/main/java/com/example/SpringSecurityJWT/models/UserEntity.java
+++ src/main/java/com/example/SpringSecurityJWT/models/UserEntity.java
@@ -10,6 +10,8 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
@@ -47,6 +49,7 @@ public class UserEntity {
 
     
     @ManyToMany(fetch = FetchType.EAGER, targetEntity = RoleEntity.class, cascade = CascadeType.PERSIST)
+   @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<RoleEntity> roles;
     
     
