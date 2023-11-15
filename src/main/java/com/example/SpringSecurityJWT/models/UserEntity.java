d4d90262257777d1191f58a324422963ed0fce55
Jonathan <Jonathan@192.168.56.1>
2023-11-15 13:46:10 -0500

    Cambios en el USER para la DB

--- src/main/java/com/example/SpringSecurityJWT/models/UserEntity.java
+++ src/main/java/com/example/SpringSecurityJWT/models/UserEntity.java
@@ -4,20 +4,15 @@
  */
 package com.example.SpringSecurityJWT.models;
 
-import jakarta.persistence.CascadeType;
+
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import java.util.Set;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -28,7 +23,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 @Entity
-@Table (name = "users")
+@Table(name = "users")
 public class UserEntity {
     
     @Id
@@ -46,11 +41,6 @@ public class UserEntity {
     
     @NotBlank
     private String password;
-
-    
-    @ManyToMany(fetch = FetchType.EAGER, targetEntity = RoleEntity.class, cascade = CascadeType.PERSIST)
-   @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
-    private Set<RoleEntity> roles;
-    
     
 }
+
